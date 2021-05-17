9ac4d70546e1855402f0fad017d6d9d0bf2806a8
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2021-05-17 16:14:55 -0300

    Fix style

--- src/components/CitySelectionList/styles.ts
+++ src/components/CitySelectionList/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-around;
-  padding: 50px;
+  padding: 50px 0;
 `;
 
 export const Button = styled("button")<{ selected: boolean }>`
